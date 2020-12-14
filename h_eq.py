35f4dec0a241189303d1c860c3b447724a7e985a
Asher Spector <amspector100@gmail.com>
2020-12-13 19:49:31 -0500

    Maybe works

--- h_eq.py
+++ h_eq.py
@@ -4,32 +4,45 @@ from scipy.optimize import minimize
 from scipy.stats import norm
 from cubature import cubature
 
+import time
+
 def hinge(t):
-    return max(t,0)
+    return np.maximum(t,0)
 
-def integrate2_normal(f,x):
-    integrand = f(x)*norm.pdf(x[0])*norm.pdf(x[1])
-    cubature(integrand)
-
-def f1(t,x,b0,g0):
-    return(hinge(t[0]+t[1]*x[0]-x[1])**2*rho_prime(b0+g0+x[0]))
-
-def f2(t,x,b0,g0):
-    return(hinge(-t[0]-t[1]*x[0]-x[1])**2*(1-rho_prime(b0+g0*x[0])))
+def integrate2_normal(f):
+    def integrand(x):
+        return f(x)*norm.pdf(x[0])*norm.pdf(x[1])
+    return cubature(
+        func=integrand, 
+        ndim=2,
+        fdim=1,
+        xmin=np.array([-8, -8]),
+        xmax=np.array([8, 8]),
+        maxEval=0,
+        relerr=1e-4,
+    )
 
 def rho_prime(t):
     return(1/(1+np.exp(-t)))
 
-
 def solve_kappa(beta0, gamma0):
     def h(t):
-        def integrand1(x):
-            return(f1(t,x,beta0,gamma0)*norm.pdf(x[0])*norm.pdf(x[1]))
-        def integrand2(x):
-            return(f2(t,x,beta0,gamma0)*norm.pdf(x[0])*norm.pdf(x[1]))
-        val1,err1 = cubature(integrand1,2,1,[-8,-8],[8,8])
-        val2,err2 = cubature(integrand2,2,1,[-8,-8],[8,8])
-        return(val1+val2)
-    return(minimize(fun = h, x0=[0,0], method = 'L-BFGS-B'))
+        # Define functions for input 
+        def f1(x):
+            hinge_output = hinge(t[0] + t[1] * x[0] - x[1])**2 
+            return hinge_output * rho_prime(beta0 + gamma0 * x[0])
+        def f2(x):
+            hinge_output = hinge(-t[0] - t[1] * x[0] - x[1])**2
+            return hinge_output * (1 - rho_prime(beta0 + gamma0 * x[0]))
+        # Normal integrations
+        time0 = time.time()
+        val1, _ = integrate2_normal(f1)
+        val2, _ = integrate2_normal(f2)
+        print(f"Cubature time is {time.time() - time0}")
+        return val1 + val2
+
+    # Minimize 
+    scipy_obj = minimize(fun = h, x0=[0,0], method = 'L-BFGS-B', options={'maxiter':2})
+    return(h(scipy_obj.x))
 
 print(solve_kappa(0,0))
