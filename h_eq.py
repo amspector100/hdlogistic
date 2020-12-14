0b2d5728494223f4c8d678252cfd77b753eaf79a
adhar1 <43148600+adhar1@users.noreply.github.com>
2020-12-13 16:04:51 -0800

    Add files via upload

--- h_eq.py
+++ h_eq.py
@@ -12,10 +12,10 @@ def integrate2_normal(f,x):
     cubature(integrand)
 
 def f1(t,x,b0,g0):
-    return(hinge(t[0]+t[1]*x[0]-x[1])^2*rho_prime(b0+g0+x[0]))
+    return(hinge(t[0]+t[1]*x[0]-x[1])**2*rho_prime(b0+g0+x[0]))
 
 def f2(t,x,b0,g0):
-    return(hinge(-t[0]-t[1]*x[0]-x[1])^2*(1-rho_prime(b0+g0*x[0])))
+    return(hinge(-t[0]-t[1]*x[0]-x[1])**2*(1-rho_prime(b0+g0*x[0])))
 
 def rho_prime(t):
     return(1/(1+np.exp(-t)))
@@ -24,9 +24,9 @@ def rho_prime(t):
 def solve_kappa(beta0, gamma0):
     def h(t):
         def integrand1(x):
-            f1(t,x,beta0,gamma0)*norm.pdf(x[0])*norm.pdf(x[1])
+            return(f1(t,x,beta0,gamma0)*norm.pdf(x[0])*norm.pdf(x[1]))
         def integrand2(x):
-            f2(t,x,beta0,gamma0)*norm.pdf(x[0])*norm.pdf(x[1])
+            return(f2(t,x,beta0,gamma0)*norm.pdf(x[0])*norm.pdf(x[1]))
         val1,err1 = cubature(integrand1,2,1,[-8,8],[8,8])
         val2,err2 = cubature(integrand2,2,1,[-8,8],[8,8])
         return(val1+val2)
