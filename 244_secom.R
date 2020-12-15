bb3af0375b6d3d3b464a76f753539c27fe9811fc
adhar1 <43148600+adhar1@users.noreply.github.com>
2020-12-15 01:32:00 -0800

    Add files via upload

--- 244_secom.R
+++ 244_secom.R
@@ -1,4 +1,5 @@
 library(glmhd)
+library(DescTools)
 Y <- secomy$V2
 index <- sample(474, 100, replace = FALSE, prob = NULL)
 print(index)
@@ -15,17 +16,20 @@ fit2 <- adjust_glm(fit, verbose = FALSE, echo = TRUE)
 s1 <- summary(fit)
 s2 <- summary(fit2)
 
-print(s1$coefficients, max = 10)
-print(s2$coefficients, max = 10)
-print(s2$coefficients[301:400])
-print(s1$coefficients[301:400])
+print(s2$coefficients[1:100])
+print(s1$coefficients[1:100])
 
-plot(seq(1,100), s2$coefficients[301:400], col = "red")
+plot(seq(1,100), s2$coefficients[301:303:400], col = "red")
 points(seq(1,100),s1$coefficients[301:400],col = "green")
 
-hist(s2$coefficients[301:400])
-hist(s1$coefficients[301:400])
-
 plot(s1$coefficients[1:100],s2$coefficients[1:100])
+
+bfit1 <- s1$coefficients[1:100]
+bfit2<- s2$coefficients[1:100]
+
+bfit1_processed <- Trim(bfit1,0.1)
+bfit2_processed <- Trim(bfit2,0.1)
+
+plot(bfit1_processed,bfit2_processed)
 abline(a = 0, b = 1)
 
