539a3020bc55e886c2e6d9affd26f67830ce18b4
adhar1 <43148600+adhar1@users.noreply.github.com>
2020-12-15 12:08:17 -0800

    Add files via upload
    call function glmhd, currently in a for loop to run many trials of covariates

--- 244_secom.R
+++ 244_secom.R
@@ -1,35 +1,52 @@
 library(glmhd)
 library(DescTools)
 Y <- secomy$V2
-index <- sample(474, 100, replace = FALSE, prob = NULL)
-print(index)
-dt <- secomX[,index]
-print(dt, max = 300)
-df <- data.frame(y = Y, x = secomX)
-fit <- glm(Y~.+0, data = dt, family = binomial, x = TRUE, y = TRUE)
-print(fit)
+# randomly samples 100 indices 
+
+glmhd <- function(){
+  index <- sample(474, 100, replace = FALSE, prob = NULL)
+  dt <- secomX[,index]
+  df <- data.frame(y = Y, x = secomX)
+  fit <- glm(Y~.+0, data = dt, family = binomial, x = TRUE, y = TRUE)
+  fit2 <- adjust_glm(fit, verbose = FALSE, echo = TRUE)
+  s1 <- summary(fit)
+  s2 <- summary(fit2)
+  # 
+  # print(s2$coefficients[1:100])
+  # print(s1$coefficients[1:100])
+  # plot(seq(1,100), s2$coefficients[301:400], col = "red")
+  # points(seq(1,100),s1$coefficients[301:400],col = "green")
+  # 
+  # hist(s1$coefficients[301:400])
+  # hist(s2$coefficients[301:400])
+  # 
+  bfit1 <- s1$coefficients[1:100]
+  bfit2<- s2$coefficients[1:100]
+  
+  inf <- s1$coefficients[1:100]/s2$coefficients[1:100]
+
+  # print(inf)
+  # bfit1_processed <- Trim(bfit1,0.1)
+  # bfit2_processed <- Trim(bfit2,0.1)
+  # 
+  # #dfrm <- s1$coefficients
+  # #write.csv(dfrm,"glm.csv")
+  # #dfrm <- s2$coefficients
+  # #write.csv(dfrm,"glm_adj.csv")
+  # 
+  # plot(bfit1, bfit2)
+  # #plot(bfit1_processed,bfit2_processed)
+  # abline(a = 0, b = 1)
+
+  retlist <- list("inf" = inf[1])
+  return(retlist)
+}
 
 
-fit2 <- adjust_glm(fit, verbose = FALSE, echo = TRUE)
-
-
-s1 <- summary(fit)
-s2 <- summary(fit2)
-
-print(s2$coefficients[1:100])
-print(s1$coefficients[1:100])
-
-plot(seq(1,100), s2$coefficients[301:303:400], col = "red")
-points(seq(1,100),s1$coefficients[301:400],col = "green")
-
-plot(s1$coefficients[1:100],s2$coefficients[1:100])
-
-bfit1 <- s1$coefficients[1:100]
-bfit2<- s2$coefficients[1:100]
-
-bfit1_processed <- Trim(bfit1,0.1)
-bfit2_processed <- Trim(bfit2,0.1)
-
-plot(bfit1_processed,bfit2_processed)
-abline(a = 0, b = 1)
+vec <- c()
+for(i in 1:50){
+  a <- glmhd()
+  print(a$inf)
+  vec<- c(vec, a$inf)
+}
 
